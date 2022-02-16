f16a2df87a562f713f8bf477f377daecfa1e75a4
Vinhadi10 <arvianh29@gmail.com>
2022-02-16 15:09:03 +0800

    first commit

---
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+
+class ProductCategoryController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('id');
+        $show_product = $request->input('show_product');
+        if($id){
+            $category = ProductCategory::with(['products'])->find($id);
+    
+            if($category){
+                return ResponseFormatter::success(
+                    $category,
+                    'Data Kategori Berhasil Diambil'
+                );
+            }
+            else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data Kategori Tidak Ada',
+                    404
+                );
+            }
+        
+    }
+    $category = ProductCategory::query();
+    if($name){
+        $category->where('name', 'like', '%' . $name . '%');
+    }
+    if ($show_product) {
+        $category->with('products');
+    }
+    return ResponseFormatter::success(
+        $category->paginate($limit),
+        'Data List kategori berhasil diambil'
+    );
+    }
+}
